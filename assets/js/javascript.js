6e8d5609cba4fa796bfec56bf4c34a9d12c6ad66
Lucero <Lucero>
2017-03-21 09:44:52 -0600

    lista

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+function agreagarFruta(){
+    var texto=document.getElementById('liname').value;
+    var li=document.createElement('LI');
+    li.innerHTML=texto==''?'(nada)':texto;
+    document.getElementById('lista').appendChild(li);
+};
