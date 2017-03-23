a777f41aa9b241111c5cd1411c47cfdd17c07f2c
Lucero <Lucero>
2017-03-23 08:59:27 -0600

    actualizacioneventos

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,21 @@
 function agreagarFruta(){
     var texto=document.getElementById('liname').value;
     var li=document.createElement('LI');
-    li.innerHTML=texto==''?'(nada)':texto;
+  if( li.innerHTML=texto==''){
+    (alert('No estas agregando una fruta?'))
+  }else{
+    li.innerHTML=texto==''?'':texto;
     document.getElementById('lista').appendChild(li);
+    document.getElementById('liname').value ="";
+  }
 };
+function borrar(){
+    lis=document.getElementById('lista').getElementsByTagName('li');
+    for(var i=0; i<lis.length;i++){
+        lis[i].onclick=function(){
+            if(confirm('¿seguro que quieres eliminar esta fruta?'))
+            this.parentNode.removeChild(this);
+        };
+    }
+
+}
